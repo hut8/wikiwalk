9cf5fb57b2bbf228893a2aac4bca7de9f552c20b
Liam Bowen <LiamBowen@gmail.com>
2022-01-22 19:08:27 -0500

    start of rust exe

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,42 @@
+#[macro_use]
+extern crate diesel;
+extern crate dotenv;
+
+use diesel::prelude::*;
+use diesel::mysql::MysqlConnection;
+use dotenv::dotenv;
+use std::env;
+
+pub fn establish_connection() -> MysqlConnection {
+    dotenv().ok();
+
+    let database_url = env::var("DATABASE_URL")
+        .expect("DATABASE_URL must be set");
+    MysqlConnection::establish(&database_url)
+        .expect(&format!("Error connecting to {}", database_url))
+}
+
+fn page_id_by_name(name: &str) -> Option<u64> {
+    
+    None
+}
+
+fn print_usage(exe: &str) {
+    println!("Usage: {} 'Source Article' 'Destination Article'", exe);
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("Wikipedia Speedrun Computer");
+    let args: Vec<String> = env::args().collect();
+    let exe = &args[0];
+
+    if args.len() != 3 {
+        print_usage(exe);
+        return;
+    }
+    let origin_title = &args[1];
+    let dest_title = &args[2];
+
+    println!("[{}] → [{}]", origin_title, dest_title);
+
 }
