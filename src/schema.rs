bddb9fa263776c9b6406a7ed8b307a402a4f7124
Liam Bowen <liambowen@gmail.com>
2022-02-01 00:24:53 +0000

    Add schema

---
+++ src/schema.rs
@@ -0,0 +1,18 @@
+table! {
+    edges (source_vertex_id, dest_vertex_id) {
+        source_vertex_id -> Int4,
+        dest_vertex_id -> Int4,
+    }
+}
+
+table! {
+    vertexes (id) {
+        id -> Int4,
+        title -> Nullable<Varchar>,
+    }
+}
+
+allow_tables_to_appear_in_same_query!(
+    edges,
+    vertexes,
+);
