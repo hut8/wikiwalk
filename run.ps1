890ed83436a2881805ad4eb9d7a3892e9d905396
Liam Bowen <liambowen@gmail.com>
2022-10-22 19:17:11 -0400

    tee log to file

--- run.ps1
+++ run.ps1
@@ -1,2 +1,4 @@
-cargo build
-.\target\debug\wikipedia-speedrun.exe build --page C:\Users\liamb\Data\enwiki-20220901-page.sql --pagelinks C:\Users\liamb\Data\pagelinks.sql
+cargo build && `
+.\target\debug\wikipedia-speedrun.exe build `
+  --page C:\Users\liamb\Data\enwiki-20220901-page.sql `
+  --pagelinks C:\Users\liamb\Data\pagelinks.sql *>&1 | Tee-Object
