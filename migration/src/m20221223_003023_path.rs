cdcc9847322231d6ae3863ecefd86e1fbc76c731
Liam Bowen <LiamBowen@gmail.com>
2022-12-23 00:36:46 +0000

    add path entity

---
+++ migration/src/m20221223_003023_path.rs
@@ -0,0 +1,48 @@
+use sea_orm_migration::prelude::*;
+
+#[derive(DeriveMigrationName)]
+pub struct Migration;
+
+#[async_trait::async_trait]
+impl MigrationTrait for Migration {
+    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .create_table(
+                Table::create()
+                    .table(Path::Table)
+                    .if_not_exists()
+                    .col(
+                        ColumnDef::new(Path::Id)
+                            .integer()
+                            .not_null()
+                            .auto_increment()
+                            .primary_key(),
+                    )
+                    .col(ColumnDef::new(Path::SourcePageId).integer().not_null())
+                    .col(ColumnDef::new(Path::TargetPageId).integer().not_null())
+                    .col(ColumnDef::new(Path::Timestamp).timestamp().not_null())
+                    .col(ColumnDef::new(Path::Duration).float().not_null())
+                    .col(ColumnDef::new(Path::PathData).json().not_null())
+                    .to_owned(),
+            )
+            .await
+    }
+
+    async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .drop_table(Table::drop().table(Path::Table).to_owned())
+            .await
+    }
+}
+
+/// Learn more at https://docs.rs/sea-query#iden
+#[derive(Iden)]
+enum Path {
+    Table,
+    Id,
+    SourcePageId,
+    TargetPageId,
+    Timestamp,
+    Duration,
+    PathData,
+}
--- wikipedia-speedrun/src/schema/mod.rs
+++ wikipedia-speedrun/src/schema/mod.rs
@@ -2,5 +2,6 @@
 
 pub mod prelude;
 
+pub mod path;
 pub mod search;
 pub mod vertex;
