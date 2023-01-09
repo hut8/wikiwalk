81855d080e9ddab53b69c4af49c2c96962cc575f
Liam Bowen <LiamBowen@gmail.com>
2023-01-09 02:52:56 -0500

    Remove useless scripts

--- scripts/build.ps1
+++
@@ -1,5 +0,0 @@
-cargo build && `
-.\target\debug\wikipedia-speedrun.exe build `
-  --page C:\Users\liamb\Data\enwiki-20220901-page.sql `
-  --pagelinks C:\Users\liamb\Data\pagelinks.sql `
-  --redirects C:\Users\liamb\Data\enwiki-20220901-redirect.sql # *>&1 | Tee-Object -FilePath "log.txt"
--- scripts/run.ps1
+++
@@ -1,4 +0,0 @@
-cargo build && `
-.\target\debug\wikipedia-speedrun.exe `
-     --data-path C:\Users\liamb\Data\speedrun-data `
-     run 'Asheville, North Carolina' 'George Washington'
