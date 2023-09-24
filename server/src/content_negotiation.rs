a83629046a5727facd160762dd33f1da375b7160
Liam Bowen <LiamBowen@gmail.com>
2023-09-24 02:42:06 -0400

    content negotiation guard

--- server/src/content_negotiation.rs
+++ server/src/content_negotiation.rs
@@ -1,12 +1,21 @@
 use actix_web::{guard::fn_guard, http::header};
-use actix_web::guard::GuardContext;
+use actix_web::guard::{GuardContext, Guard};
 
 
-fn accept_json_guard(ctx: &GuardContext) {
+fn accept_json_guard(ctx: &GuardContext) -> impl Guard {
     fn_guard(|ctx| {
         match ctx.header::<header::Accept>() {
             Some(hdr) => hdr.preference() == "application/json",
             None => false
         }
     })
+}
+
+fn accept_html_guard(ctx: &GuardContext) -> impl Guard {
+  fn_guard(|ctx| {
+      match ctx.header::<header::Accept>() {
+          Some(hdr) => hdr.preference() == "text/html",
+          None => false
+      }
+  })
 }
