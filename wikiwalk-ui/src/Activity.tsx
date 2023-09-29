40cdf7657e5f3c7081bba4dd540e66e3c96b0ed0
Liam Bowen <liam@supervillains.io>
2023-09-29 15:48:27 -0400

    add useful old code from svelte impl

---
+++ wikiwalk-ui/src/Activity.tsx
@@ -0,0 +1,15 @@
+import './Activity.css';
+
+export function Activity() {
+  return (
+    <div className="la-pacman la-dark la-3x">
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+    </div>
+  )
+
+}
