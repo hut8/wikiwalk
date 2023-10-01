ccb128fcb634c79dca43527f05adf1ab6548b614
Liam Bowen <LiamBowen@gmail.com>
2023-10-01 03:07:23 -0400

    exception for curl on content negotiation

--- server/src/content_negotiation.rs
+++ server/src/content_negotiation.rs
@@ -2,9 +2,13 @@ use actix_web::guard::GuardContext;
 use actix_web::http::header;
 
 pub fn accept_json_guard(ctx: &GuardContext) -> bool {
+    // Either they explicitly ask for JSON, or they're using curl
     match ctx.header::<header::Accept>() {
         Some(hdr) => hdr.preference() == "application/json",
-        None => false,
+        None => match ctx.head().headers().get("user-agent") {
+            Some(hdr) => hdr.to_str().unwrap_or_default().contains("curl"),
+            None => false,
+        },
     }
 }
 
