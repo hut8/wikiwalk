ef7c031d9d77853f834309319e70afc392bb8baf
Liam Bowen <liam@supervillains.io>
2023-10-04 14:54:45 -0400

    Make JSON the default

--- server/src/content_negotiation.rs
+++ server/src/content_negotiation.rs
@@ -2,13 +2,10 @@ use actix_web::guard::GuardContext;
 use actix_web::http::header;
 
 pub fn accept_json_guard(ctx: &GuardContext) -> bool {
-    // Either they explicitly ask for JSON, or they're using curl
+    // This makes JSON the default if no Accept header is present.
     match ctx.header::<header::Accept>() {
         Some(hdr) => hdr.preference() == "application/json",
-        None => match ctx.head().headers().get("user-agent") {
-            Some(hdr) => hdr.to_str().unwrap_or_default().contains("curl"),
-            None => false,
-        },
+        None => true,
     }
 }
 
