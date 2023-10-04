6c3b496166ef14a71d8a776ef142e67181a96e17
Liam Bowen <liam@supervillains.io>
2023-10-04 15:32:32 -0400

    Fix activity formatting

--- wikiwalk-ui/src/Activity.tsx
+++ wikiwalk-ui/src/Activity.tsx
@@ -1,15 +1,18 @@
+import { Box } from '@mui/material';
 import './Activity.css';
 
 export function Activity() {
   return (
-    <div className="la-pacman la-dark la-3x">
-      <div />
-      <div />
-      <div />
-      <div />
-      <div />
-      <div />
-    </div>
+    <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '50vh', flexDirection: 'column' }}>
+      <div className="la-pacman la-dark la-3x">
+        <div />
+        <div />
+        <div />
+        <div />
+        <div />
+        <div />
+      </div>
+    </Box>
   )
 
 }
