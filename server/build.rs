5d11d6749de527aea9ac217792b1abdbb445245a
Liam Bowen <liam@supervillains.io>
2023-10-06 12:55:12 -0400

    add option to skip frontend build

--- server/build.rs
+++ server/build.rs
@@ -1,9 +1,14 @@
 use static_files::NpmBuild;
 
 fn main() {
+    if std::env::var("WIKIWALK_SKIP_FRONTEND_BUILD").is_ok() {
+        return;
+    }
     NpmBuild::new("../wikiwalk-ui")
-        .install().unwrap() // runs npm install
-        .run("build").unwrap() // runs npm run build
+        .install()
+        .unwrap() // runs npm install
+        .run("build")
+        .unwrap() // runs npm run build
         .target("../wikiwalk-ui/dist")
         .change_detection()
         .to_resource_dir()
