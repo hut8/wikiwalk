8ac7ff8e9c3e0e6d0b3c7702d0e686973ed8ab76
Liam Bowen <liam@supervillains.io>
2023-11-27 19:15:18 -0500

    fix content negotiation build

--- server/src/content_negotiation.rs
+++ server/src/content_negotiation.rs
@@ -5,7 +5,7 @@ pub fn accept_json_guard(ctx: &GuardContext) -> bool {
     // This makes JSON the default if no Accept header is present.
     match ctx.header::<header::Accept>() {
         Some(hdr) => {
-          match hdr.preference() {
+          match hdr.preference().essence_str() {
             "application/json" => true,
             "*/*" => true, // default, e.g., for curl
             _ => false,
