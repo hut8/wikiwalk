b37b7407fa844232ce9031c638747414de5ab84e
Liam Bowen <liam@supervillains.io>
2024-05-22 15:06:43 -0400

    add migration for link target

--- migration/src/lib.rs
+++ migration/src/lib.rs
@@ -2,6 +2,7 @@ pub use sea_orm_migration::prelude::*;
 
 mod m20221201_000001_search;
 mod m20221223_003023_path;
+mod m20240522_190300_add_link_targets;
 
 pub struct Migrator;
 
@@ -11,6 +12,7 @@ impl MigratorTrait for Migrator {
         vec![
             Box::new(m20221201_000001_search::Migration),
             Box::new(m20221223_003023_path::Migration),
+            Box::new(m20240522_190300_add_link_targets::Migration),
         ]
     }
 }
